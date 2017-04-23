98a5946a3a053af56a1e806b6bf3ede8fd8b0518
Esrom Cataneo <esromgalaxy5@gmail.com>
2017-04-23 19:25:55 -0300

    XOSP-ify

--- xosp.mk
+++ xosp.mk
@@ -14,13 +14,16 @@
 
 $(call inherit-product, device/motorola/merlin/full_merlin.mk)
 
+# Inherit some common XOSP stuff.
+$(call inherit-product, vendor/xosp/config/xosp.mk)
+
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlin
-PRODUCT_NAME := lineage_merlin
+PRODUCT_NAME := xosp_merlin
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := merlin
